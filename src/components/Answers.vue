b23376abbde3f460a56174cb482c78858ed663e7
jorgemartiinez19 <jorgemartiinez19@gmail.com>
2020-05-28 12:18:31 +0200

    vamos a ver como refactorizar esta mierda

---
+++ src/components/Answers.vue
@@ -0,0 +1,5 @@
+<template> <div>Answers</div></template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/Questions.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="question in quizz.questions" :key="question.id">
+      {{ question.question }}
+      <p v-if="submit">{{ checkAndShow(question.id) }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  computed: {
+    questions() {
+      return this.$store.currentQuizz.questions;
+    }
+  }
+};
+</script>
